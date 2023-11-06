8aa3eee57faa9e254ba14bec487d5c84c2027af9
Dawid Perkowski <87314459+DawidPerkowskiGit@users.noreply.github.com>
2023-11-06 12:57:31 +0100

    Feature: Setup basic layout (#4)
    * feature: cloned main repository, added .gitignore file
    
    * feature: added Maven as a project build tool, implemented basic project structure
    
    * feature: added Thymeleaf as template engine, implemented simple homepage with header

--- src/main/java/dpapps/controller/MainAppController.java
+++ src/main/java/dpapps/controller/MainAppController.java
@@ -1,6 +1,8 @@
 package dpapps.controller;
 
 import dpapps.constants.MessageConstants;
+import dpapps.service.MainAppControllerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,8 +10,35 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MainAppController {
 
+    private final MainAppControllerService controller;
+
+    @Autowired
+    MainAppController(MainAppControllerService controller) {
+        this.controller = controller;
+    }
+
+    /**
+     * Applications health check endpoint
+     */
     @GetMapping("/health")
     public @ResponseBody String healthCheck() {
-        return MessageConstants.APP_IS_RUNNING;
+        return controller.getHealthCheck();
     }
+
+    /**
+     * Index page endpoint
+     */
+    @GetMapping("/")
+    public String homePageSlash() {
+        return controller.getHomePage();
+    }
+
+    /**
+     * Index page endpoint
+     */
+    @GetMapping("")
+    public String homePage() {
+        return controller.getHomePage();
+    }
+
 }
