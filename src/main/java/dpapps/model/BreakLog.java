02fd8e5859be1cb9d02b71a491de5967e12bad19
DawidPerkowskiGit <dtperkowski@gmail.com>
2023-12-12 12:06:22 +0100

    feat: breaks are stored in the database

---
+++ src/main/java/dpapps/model/BreakLog.java
@@ -0,0 +1,36 @@
+package dpapps.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "break")
+public class BreakLog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "start_date")
+    private LocalDate startDate;
+
+    @Column(name = "start_time", columnDefinition = "TIME")
+    private LocalTime startTime;
+
+    @Column(name = "end_date")
+    private LocalDate endDate;
+
+    @Column(name = "end_time", columnDefinition = "TIME")
+    private LocalTime endTime;
+
+    public BreakLog() {}
+}
